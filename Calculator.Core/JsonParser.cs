80f72c0a0f6b236bf9044124140c4b01e519a14f
Ilia <ilia@kucherov.net>
2024-04-01 21:43:06 +1100

    Parse addition from json

--- Calculator.Core/JsonParser.cs
+++ Calculator.Core/JsonParser.cs
@@ -1,4 +1,7 @@
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Calculator.Core.Operations;
 
 namespace Calculator.Core;
 
@@ -6,6 +9,34 @@ public class JsonParser : IToExpressionParser<JsonNode>
 {
     public Expression Parse(JsonNode parsable)
     {
-        throw new NotImplementedException();
+        var maths = parsable["Maths"];
+        var expressionDto = maths["Operation"].Deserialize<ExpressionDto>();
+        return ParseExpression(expressionDto);
+    }
+
+    private Expression ParseExpression(ExpressionDto expressionDto)
+    {
+        var operationName = expressionDto.ID;
+        var operation = Operation.FromName(operationName);
+        var subexpressions = expressionDto
+            .Value
+            .Select(double.Parse)
+            .Select(Expression.CreateSingleValued)
+            .ToList();
+        if (expressionDto.Operation != null)
+        {
+            var subexpression = ParseExpression(expressionDto.Operation);
+            subexpressions.Add(subexpression);
+        }
+
+        return Expression.CreateNested(subexpressions, operation);
+    }
+
+    private class ExpressionDto
+    {
+        [JsonPropertyName("@ID")]
+        public string ID { get; set; }
+        public List<string> Value { get; set; }
+        public ExpressionDto? Operation { get; set; }
     }
 }
